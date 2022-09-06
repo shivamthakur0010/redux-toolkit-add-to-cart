b137a5974b5c9c9d19d75b6b949397333e0a49fb
shivamthakur0010 <demonking0010@gmail.com>
2022-09-06 21:58:29 +0530

    ready for deploy

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import {useDispatch, useSelector} from 'react-redux'
+import {useEffect} from 'react';
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
+import Model from "./components/Model";
+function App() {
+  const {cartItems, isLoading} = useSelector(store=> store.cart);
+  const {isOpen} = useSelector(store=> store.modal);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(calculateTotals())
+  },[cartItems, dispatch]);
+
+  useEffect(() =>{
+    dispatch(getCartItems());
+  },[dispatch])
+
+  if(isLoading){
+    return <div className="loading">
+      <h1>Loading...</h1>
+    </div>
+  }
+
+  return <main>
+    {isOpen && <Model/>}
+    <Navbar/>
+    <CartContainer/>
+  </main>;
+}
+export default App;
